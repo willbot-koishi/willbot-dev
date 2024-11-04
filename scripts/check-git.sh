82b601d840a03e19d4afc49feb432a256b1a0e23
ForkΨKILLET <fork_killet@outlook.com>
2024-11-05 03:42:02 +0800

    archive: 20241105

--- scripts/check-git.sh
+++ scripts/check-git.sh
@@ -5,7 +5,30 @@ for WS in external/*; do
 	cd $WS
 	GIT_OUTPUT="$(git status)"
 	[[ "$GIT_OUTPUT" =~ "无文件要提交" ]] || {
-		echo -n "$WS "
+		if [[ "$1" == i ]]; then
+			while true; do
+				clear
+				echo -n "What to do with $WS? (d=diff/D=~cached/c=commit/q=quit/n=next) "
+				read -n 1 res; echo
+				case $res in
+					(q) exit 0;;
+					(d) git diff;;
+					(D) git diff --cached;;
+					(c) git add .
+						echo -n "Commit message: "
+						read msg
+						git commit -m "$msg"
+						git log -1 HEAD
+						echo -n "Push? (y/n) "
+						read -n 1 res; echo
+						[[ "$res" = y ]] && git push
+						;;
+					(*) break
+				esac
+			done
+		else
+			echo -n "$WS "
+		fi
 	}
 	cd "$CWD"
 done
